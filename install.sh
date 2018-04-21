990ace87bfb0e3816f56836165b8368bef44229d
Vo Trung Chi <votrungchi_108@live.com>
2018-04-21 15:16:20 +0700

    Backup current system configurations before do installation.

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 minevim_dir="$(pwd)"
 
+#backup current configurations
+mv ~/.vimrc ~/.vimrc.backup
+mv ~/.vim ~/.vim.backup
+mv ~/.tmux.conf ~/.tmux.conf.backup
+
+#create softlink to new configurations
 ln -s ${minevim_dir}/.vimrc ~/.vimrc
 ln -s ${minevim_dir}/.vim ~/.vim
 ln -s ${minevim_dir}/.tmux.conf ~/.tmux.conf
