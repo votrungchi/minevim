36fe7b03388127dcb547f4a13687204ed4c63e75
Chi Vo <chivo@Chis-MacBook-Pro.local>
2018-09-29 19:07:24 +0700

    remove tmux.conf
    Use the tmux-config on github instead.
    
    Signed-off-by: Chi Vo <chivo@Chis-MacBook-Pro.local>

--- install.sh
+++ install.sh
@@ -4,14 +4,12 @@ minevim_dir="$(pwd)"
 #backup current configurations
 mv ~/.vimrc ~/.vimrc.backup
 mv ~/.vim ~/.vim.backup
-mv ~/.tmux.conf ~/.tmux.conf.backup
 
 #create softlink to new configurations
 ln -s ${minevim_dir}/.vimrc ~/.vimrc
 ln -s ${minevim_dir}/.vim ~/.vim
-ln -s ${minevim_dir}/.tmux.conf ~/.tmux.conf
 
-#initialize plugins 
+#initialize plugins
 git submodule update --init --recursive
 
 # install YCM for Ubuntu Linux x64
